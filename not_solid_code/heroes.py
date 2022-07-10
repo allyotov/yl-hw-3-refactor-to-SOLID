d77aeb25e2cf414a7e5b6d6721fc83abe8542e31
Alexander Lyotov <sippie84@gmail.com>
2022-07-10 16:16:30 +0300

    feature/created mixins for unique ultimate attacks

--- not_solid_code/heroes.py
+++ not_solid_code/heroes.py
@@ -11,48 +11,24 @@ class SuperHero:
     def find(self, place):
         self.finder.get_antagonist(place)
 
-    # Проблема: Герой не должен заниматься оповещениями о своей победе, это задача масс-медиа.
-    # Несоблюден: Принцип единой ответственности.
-    # По SOLID: Вынести оповещение в отдельный класс, занимающийся выводом информации.
-    # Когда возникнут трудности? Добавьте оповещение о победе героя через газеты или через TV (на выбор)
-    # а также попробуйте оповестить планеты (у которых вместа атрибута name:str используется coordinates:List[float]).
-    def create_news(self, place):
-        place_name = getattr(place, 'name', 'place')
-        print(f'{self.name} saved the {place_name}!')
-
-    # Проблема: Для каждого супергероя реализованы все методы обращения с оружием.
-    # Несоблюден: Принцип разделения интерфейса
-    # По SOLID: Создать классы-миксины для каждого оружия
-    # Когда возникнут трудности? Попробуйте запретить Чаку норрису пользоваться лазерами из глаз!
     def fire_a_gun(self):
         print('PIU PIU')
-
-    def incinerate_with_lasers(self):
-        print('Wzzzuuuup!')
-
-    def roundhouse_kick(self):
-        print('Bump')
 
     def attack(self):
         self.fire_a_gun()
 
-    # Проблема: У разных супергероев разные суперспособности
-    # Несоблюден: Принцип открытости/закрытости
-    # По SOLID: Каждого супергероя реализовать как наследника SuperHero и вместо изменения базового класса переопределять нужные методы
-    # Когда возникнут трудности? Когда в вашем коде поселится вся команда Мстителей
+class KickMixin:
     def ultimate(self):
-        if self.name == 'Clark Kent':
-            self.incinerate_with_lasers()
+        print('Bump')
 
+class LazerMixin:
+    def ultimate(self):
+        print('Wzzzuuuup!')
 
-class Superman(SuperHero):
+class Superman(LazerMixin, SuperHero):
 
     def __init__(self):
         super(Superman, self).__init__('Clark Kent', True)
 
-    # Проблема: Сигнатура метода изменилась. Если мэр города обратится к супермену как к супергерою у Кларка возникнут проблемы с атакой
-    # Несоблюден: Принцип подстановки Барбары Лисков
-    # По SOLID: Не допускать таких вольностей
-    # Когда возникнут трудности? При первой же битве
-    def attack(self):
+    def kick(self):
         return 'Kick'
